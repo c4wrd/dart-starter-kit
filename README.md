0a0080701f5564fee60214a50e1e8e31ebb70c3c
Cory Forward <cforward@dataminr.com>
2017-02-18 13:28:19 -0700

    Initial upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# Dart Starter Kit
+
+This is a starter kit for building web applications with Dart. This includes:
+ - Angular 2
+ - Polymer
+ - Polymer Elements
+
+Currently this is a work in progress.
---
+++ lib/app_component.dart
@@ -0,0 +1,12 @@
+// Copyright (c) 2017, cory. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:angular2/core.dart';
+
+@Component(
+  selector: 'main-app',
+  templateUrl: 'app_component.html'
+)
+class AppComponent {
+
+}
