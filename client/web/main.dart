91df3e0217edf6d5136ef2b456ae36c6a9db7d34
Cory Forward <cforward@dataminr.com>
2017-02-21 23:16:26 -0700

    Fixed pubspec, build wasn't completing

--- client/web/main.dart
+++ client/web/main.dart
@@ -2,11 +2,12 @@ import 'dart:html';
 
 // import packages required for polymer and polymer elements
 // import Angular 2
+import 'package:angular2/angular2.dart';
 import 'package:angular2/platform/browser.dart';
 
 // import the components for the application
 import 'package:myclient/app_component.dart';
 
-main() async {
+main() {
   bootstrap(AppComponent);
 }
